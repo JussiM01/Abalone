00484bb46dff630968c55fd1d28b708b9c65a660
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-26 16:39:42 +0300

    Converted data

--- abalone_linear1.py
+++ abalone_linear1.py
@@ -1,13 +1,16 @@
-import numpy as
-import pandas as import pd
-import matplolib as mpl
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import seaborn as sb
 from readdata import *
 
 sb.set(color_codes=True)
 
 data = abalone()
+data_conv = data[['Length', 'Diameter', 'Height', 'Whole Weight',
+    'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']].astype('float64')
 
-sb.regplot(x='Length', y='Diameter', data=data)
+sb.regplot(x='Length', y='Diameter', data=data_conv)
 
 plt.show()
