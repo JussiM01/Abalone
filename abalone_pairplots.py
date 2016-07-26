1aab074f327208a4930f14274a29dc8b4c6db273
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-26 15:41:34 +0300

    Added different types of pair plots

--- abalone_pairplots.py
+++ abalone_pairplots.py
@@ -15,3 +15,34 @@ data2 = data[['Length', 'Diameter', 'Height', 'Rings', 'Sex']]
 sb.pairplot(data2, hue='Sex')
 
 plt.show()
+
+####################
+
+data3 = data[['Length', 'Diameter', 'Height', 'Rings']]
+
+sb.pairplot(data3)
+
+plt.show()
+
+data4 = data[['Length', 'Diameter', 'Height', 'Rings', 'Sex']]
+
+sb.pairplot(data4, hue='Sex')
+
+plt.show()
+
+####################
+data1 = data[['Length', 'Diameter', 'Height', 'Rings']]
+
+g = sb.PairGrid(data1)
+g.map_diag(sb.kdeplot)
+g.map_offdiag(sb.kdeplot, cmap="Blues_d", n_levels=6)
+
+plt.show()
+
+data2 = data[['Length', 'Diameter', 'Height', 'Rings', 'Sex']]
+
+g = sb.PairGrid(data2, hue='Sex')
+g.map_diag(sb.kdeplot)
+g.map_offdiag(sb.kdeplot, cmap="Blues_d", n_levels=6)
+
+plt.show()
