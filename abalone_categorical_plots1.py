71f55cf3646846abc1818cc124ac3e81618aa122
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-27 16:04:14 +0300

    Made some swarm plots and experimented with point size

--- abalone_categorical_plots1.py
+++ abalone_categorical_plots1.py
@@ -8,6 +8,18 @@ sb.set(style="whitegrid", color_codes="True")
 
 data = abalone()
 
-sb.stripplot(x='Rings', y='Diameter', data=data)
+sb.stripplot(x='Rings', y='Diameter', data=data, size=2)
+
+plt.show()
+
+sb.stripplot(x='Rings', y='Diameter', data=data, jitter=True, size=2)
+
+plt.show()
+
+sb.swarmplot(x='Rings', y='Diameter', data=data, size=2)
+
+plt.show()
+
+sb.swarmplot(x='Rings', y='Diameter', data=data, size=1)
 
 plt.show()
