d458887312437d5363a48f0d11899e31bb2ba1e0
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-28 17:00:45 +0300

    Added a function for splitting the data for cross validating

--- readdata.py
+++ readdata.py
@@ -1,7 +1,12 @@
 import pandas as pd
+from sklearn.cross_validation import train_test_split
 
 def abalone():
     data = pd.read_csv('abalone.data', header=None)
     data.columns = ['Sex', 'Length', 'Diameter', 'Height', 'Whole Weight',
                     'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']
     return data
+
+def abalone_train_test():
+    data = abalone()
+    return train_test_split(data, random_state = 1)
