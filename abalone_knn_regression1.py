239fe98b1cdcb0b85a6129fd9765b8407f6bdb94
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-28 19:37:19 +0300

    Added 2 files with KNN regression

---
+++ abalone_knn_regression1.py
@@ -0,0 +1,25 @@
+from sklearn import neighbors
+from readdata import *
+from sklearn.metrics import mean_squared_error
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+X_vars = ['Length', 'Whole Weight']
+
+y_vars = ['Rings']
+
+X_train, X_test, y_train, y_test = abalone_train_test(X_vars, y_vars)
+
+model = neighbors.KNeighborsRegressor(n_neighbors=10)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print('mean squared error of prediction:', mean_squared_error(y_test, y_pred))
+
+comparison = y_test.copy()
+comparison['Prediction'] = y_pred
+
+sb.jointplot(x = 'Rings', y = 'Prediction', xlim=(0,25), ylim=(0,25), data=comparison)
+
+plt.show()
---
+++ abalone_knn_regression2.py
@@ -0,0 +1,26 @@
+from sklearn import neighbors
+from readdata import *
+from sklearn.metrics import mean_squared_error
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+X_vars = ['Length', 'Whole Weight']
+
+y_vars = ['Rings']
+
+X_train, X_test, y_train, y_test = abalone_train_test(X_vars, y_vars)
+
+for n in [1, 5, 10, 20]:
+    model = neighbors.KNeighborsRegressor(n_neighbors=n)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    print('mean squared error of prediction:', mean_squared_error(y_test, y_pred))
+
+    comparison = y_test.copy()
+    comparison['Prediction'] = y_pred
+
+    sb.jointplot(x = 'Rings', y = 'Prediction', xlim=(0,25), ylim=(0,25), data=comparison)
+
+plt.show()
