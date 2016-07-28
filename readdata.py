bd904490b6650c7923353d1fa2649be238648547
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-28 17:13:22 +0300

    Changed the definition of the splitting function

--- readdata.py
+++ readdata.py
@@ -7,6 +7,8 @@ def abalone():
                     'Shucked Weight', 'Viscera Weight', 'Shell Weight', 'Rings']
     return data
 
-def abalone_train_test():
+def abalone_train_test(X_vars, y_vars):
     data = abalone()
-    return train_test_split(data, random_state = 1)
+    X = data[X_vars]
+    y = data[y_vars]
+    return train_test_split(X, y, random_state = 1)
