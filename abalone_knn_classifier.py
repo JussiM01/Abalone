c1cc764c1a717bcc01393b5a110b2aef9e10ec0a
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-30 17:58:35 +0300

    Added a file for KNN classifier

---
+++ abalone_knn_classifier.py
@@ -0,0 +1,23 @@
+from sklearn import neighbors
+from readdata import *
+from sklearn.metrics import accuracy_score
+from statplots import *
+
+X_vars = ['Length', 'Whole Weight']
+
+y_vars = 'Infant'
+
+X_train, X_test, y_train, y_test = abalone_train_test(X_vars, y_vars)
+
+model = neighbors.KNeighborsClassifier(n_neighbors=20)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+y_pred_proba = model.predict_proba(X_test)
+
+print('accuracy score of prediction:', accuracy_score(y_test, y_pred))
+
+confusion_matrix(y_test, y_pred)
+roc_curve(y_test, y_pred_proba)
+decision_boundary(model, X_test, y_test)
+heatmap(model, X_test, y_test)
