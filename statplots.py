fba7d67964ee973c52d79d4fd9c0167a3b3efe68
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-30 17:44:01 +0300

    Added a heatmap function

--- statplots.py
+++ statplots.py
@@ -39,3 +39,21 @@ def decision_boundary(model, X_test, y_test, grid_size=200):
     plt.scatter(X_test[rows0, 0], X_test[rows0, 1], color='k', s=1)
     plt.scatter(X_test[rows1, 0], X_test[rows1, 1], color='w', s=1)
     plt.show()
+
+def heatmap(model, X_test, y_test, grid_size=200):
+    X_test = X_test.values
+    xmin, xmax = X_test[:, 0].min(), X_test[:, 0].max()
+    ymin, ymax = X_test[:, 1].min(), X_test[:, 1].max()
+    xstep, ystep = (xmax -xmin)/grid_size, (ymax -ymin)/grid_size
+    xr = np.arange(xmin, xmax, xstep)
+    yr = np.arange(ymin, ymax, ystep)
+    xgrid, ygrid =  np.meshgrid(xr, yr)
+    pred = model.predict_proba(np.c_[xgrid.ravel(), ygrid.ravel()])[:,1].reshape(xgrid.shape)
+    plt.figure()
+    plt.pcolormesh(xgrid, ygrid, pred)
+    plt.colorbar()
+    rows0 = np.where(y_test == 0)
+    rows1 = np.where(y_test == 1)
+    plt.scatter(X_test[rows0, 0], X_test[rows0, 1], color='k', s=1)
+    plt.scatter(X_test[rows1, 0], X_test[rows1, 1], color='w', s=1)
+    plt.show()
