49f3bc535f6a765a6232f9fdeef21b7a651c0e22
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-30 18:38:50 +0300

    Added a with linear support vector machine classifier

---
+++ abalone_svm_linear_classifier.py
@@ -0,0 +1,20 @@
+from sklearn import svm
+from readdata import *
+from sklearn.metrics import accuracy_score
+from statplots import *
+
+X_vars = ['Length', 'Whole Weight']
+
+y_vars = 'Infant'
+
+X_train, X_test, y_train, y_test = abalone_train_test(X_vars, y_vars)
+
+model = svm.SVC(kernel='linear')
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print('accuracy score of prediction:', accuracy_score(y_test, y_pred))
+
+confusion_matrix(y_test, y_pred)
+decision_boundary(model, X_test, y_test)
